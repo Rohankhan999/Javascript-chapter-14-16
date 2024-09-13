b8904dfb5c8a0ae219e48a846b040851436c5d7a
Shoaib Hameed <>
2024-09-13 12:01:09 +0500

    y

---
+++ app.js
@@ -0,0 +1,22 @@
+// Assigment no 05//(Array)
+
+//question no 1//
+
+// var Futurestudents = [] ;
+//  Futurestudents = [""];
+// var stringArray = ["Rohan","ali","khan","irfan"];
+// var numArray = [3,4,5,79,89];
+// var bhooleanArray = [true,false,true];
+// var mixedArray = ["ali",34,true,null];  
+
+//question no 7//
+
+// var Qualification = ["SSC","HSC","BSc","BS","BCOM","MS","M.Phil","Phd"];
+// document.write("<h1>Qualification</h1>"
+//      + Qualification[0] + "<br>");
+// document.write(  Qualification[1] + "<br>");
+// document.write( Qualification[2] + "<br>");
+// document.write( Qualification[3] + "<br>");
+// document.write( Qualification[4] + "<br>");
+// document.write( Qualification[5] + "<br>");
+// document.write( Qualification[6] + "<br>");
